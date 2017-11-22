d29c552e7660b7e98138e3c5b0971cbf02b982f6
Zejian Shen <zesansan@gmail.com>
2017-11-22 09:13:06 -0800

    math functions work, but need to reflect in querystring

--- Unit-01/02-flask-routing/app.py
+++ Unit-01/02-flask-routing/app.py
@@ -3,24 +3,49 @@ from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
 @app.route("/home")
-def home():
+def index():
 	return render_template("index.html")
 
-@app.route("/add")
+@app.route("/add", methods = ["GET", "POST"])
 def add():
-	return render_template("add.html")
+	
+	if request.method == "POST":
+		num1 = int(request.form['add_num1'])
+		num2 = int(request.form['add_num2'])
+		add_result = num1 + num2
+		return render_template("add.html", add_result = add_result)
+	elif request.method == 'GET':
+		return render_template("add.html")
 
-@app.route("/subtract")
-def subract():
-	return render_template("subract.html")
+@app.route("/subtract", methods =['GET', 'POST'])
+def subtract():
+	if request.method == "POST":
+		num1 = int(request.form['sub_num1'])
+		num2 = int(request.form['sub_num2'])
+		sub_result = num1 - num2
+		return render_template("subtract.html", sub_result = sub_result)
+	elif request.method == 'GET':
+		return render_template("subtract.html")
 
-@app.route("/multiply")
+@app.route("/multiply", methods =['GET', 'POST'])
 def multiply():
-	return render_template("multiply.html")
+	if request.method == "POST":
+		num1 = int(request.form['mult_num1'])
+		num2 = int(request.form['mult_num2'])
+		mult_result = num1 * num2
+		return render_template("multiply.html", mult_result = mult_result)
+	elif request.method == 'GET':
+		return render_template("multiply.html")
 
-@app.route("/divide")
+@app.route("/divide", methods =['GET', 'POST'])
 def divide():
-	return render_template("divide.html")
+	if request.method == "POST":
+		num1 = int(request.form['divide_num1'])
+		num2 = int(request.form['divide_num2'])
+		divide_result = num1 / num2
+		return render_template("divide.html", divide_result = divide_result)
+	elif request.method == 'GET':
+		return render_template("divide.html")
 
 if __name__=="__main__":
 	app.run(debug=True)

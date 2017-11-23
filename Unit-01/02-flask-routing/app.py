cd716d7cd46acb40df878a3fa239dc611acc548b
Zejian Shen <zesansan@gmail.com>
2017-11-22 20:54:17 -0800

    completed all routing except bonus

--- Unit-01/02-flask-routing/app.py
+++ Unit-01/02-flask-routing/app.py
@@ -1,51 +1,28 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/home")
-def index():
-	return render_template("index.html")
+@app.route("/add/<int:add_num1>/<int:add_num2>")
+def add(add_num1, add_num2):
+	return "The sum is {}".format(add_num1 + add_num2)
 
-@app.route("/add", methods = ["GET", "POST"])
-def add():
-	
-	if request.method == "POST":
-		num1 = int(request.form['add_num1'])
-		num2 = int(request.form['add_num2'])
-		add_result = num1 + num2
-		return render_template("add.html", add_result = add_result)
-	elif request.method == 'GET':
-		return render_template("add.html")
+@app.route("/subtract/<int:subtract_num1>/<int:subtract_num2>")
+def subtract(subtract_num1, subtract_num2):
+		return "The difference is {}".format(subtract_num1 - subtract_num2) 
 
-@app.route("/subtract", methods =['GET', 'POST'])
-def subtract():
-	if request.method == "POST":
-		num1 = int(request.form['sub_num1'])
-		num2 = int(request.form['sub_num2'])
-		sub_result = num1 - num2
-		return render_template("subtract.html", sub_result = sub_result)
-	elif request.method == 'GET':
-		return render_template("subtract.html")
+@app.route("/multiply/<int:mult_num1>/<int:mult_num2>")
+def multiply(mult_num1, mult_num2):
+	return "The product is {}".format(mult_num1 * mult_num2)
 
-@app.route("/multiply", methods =['GET', 'POST'])
-def multiply():
-	if request.method == "POST":
-		num1 = int(request.form['mult_num1'])
-		num2 = int(request.form['mult_num2'])
-		mult_result = num1 * num2
-		return render_template("multiply.html", mult_result = mult_result)
-	elif request.method == 'GET':
-		return render_template("multiply.html")
+@app.route("/divide/<int:divide_num1>/<int:divide_num2>")
+def divide(divide_num1, divide_num2):
+	return "The quotient is {}".format(divide_num1 / divide_num2)
 
-@app.route("/divide", methods =['GET', 'POST'])
-def divide():
-	if request.method == "POST":
-		num1 = int(request.form['divide_num1'])
-		num2 = int(request.form['divide_num2'])
-		divide_result = num1 / num2
-		return render_template("divide.html", divide_result = divide_result)
-	elif request.method == 'GET':
-		return render_template("divide.html")
+@app.route("math/")
+def math(math_add_num1, math_add_num2):
+	return "The sum is {}".format(math_add_num1 + math_add_num2)
 
 if __name__=="__main__":
 	app.run(debug=True)
+
+

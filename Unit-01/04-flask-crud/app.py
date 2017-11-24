a3c17f449d80f904bfa29beccf928cb5e8d79272
Zejian Shen <zesansan@gmail.com>
2017-11-24 15:54:31 -0800

    basic template

--- Unit-01/04-flask-crud/app.py
+++ Unit-01/04-flask-crud/app.py
@@ -1,3 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from snack import Snack
+from flask_modus import Modus
 
-snack_list = []
+snacks=[
+	Snack('potato chips', 'savory'),
+	Snack('beef jerky', 'savory'),
+	Snack('chocolate chip cookies', 'sweet'),
+	Snack('granola bar', 'health')
+
+]
+
+app = Flask(__name__)
+modus = Modus(app)
+
+@app.route('/snacks', methods=["GET", "POST"])
+def index():
+	return render_template('index.html', snacks = snacks)
+
+@app.route('/snacks/new')
+def new():
+	return render_template('new.html')
+
+@app.route('/snacks/:id')
+def show():
+	return render_template('show.html')
+
+@app.route('/snacks/:id/edit')
+def edit():
+	return render_template('edit.html')			
+
+if __name__ == '__main__':
+	app.run(debug=True)	
+
+		
--- Unit-01/04-flask-crud/snack.py
+++ Unit-01/04-flask-crud/snack.py
@@ -1,1 +1,11 @@
 # Add a class for a snack here!
+class Snack:
+	count = 1
+	def __init__(self, name, kind):
+		self.name = name
+		self.kind = kind
+		self.id = Snack.count
+		Snack.count += 1
+
+	def __repr__(self):
+		return f'Snack#:{self.id}; Name:{self.name}; Kind:{self.kind}'	

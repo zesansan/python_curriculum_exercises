44921f0d64e8677a83430671822b8eb4abbad781
Zejian Shen <zesansan@gmail.com>
2017-11-24 14:07:58 -0800

    /math querystring works

--- Unit-01/03-templating/app.py
+++ Unit-01/03-templating/app.py
@@ -2,22 +2,28 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/calculator")
+@app.route("/calculate")
 def calc():
-		return render_template("calc.html")
+	return render_template("calc.html")
 
-@app.route("/math", methods =['POST'])
-def math():
-	num1 = int(request.form['num1'])
-	num2 = int(request.form['num2'])
-	if request.form['math']=='add': 
-		return "the sum is {}".format(num1 + num2)
-	elif request.form['math'] == 'subtract':
-		return "the difference is {}".format(num1 - num2)
-	elif request.form['math'] == 'multiply':
-		return "the product is {}".format(num1 * num2)
-	elif request.form['math'] == 'divide':
-		return "the quotient is {}".format(num1 / num2)			
+@app.route("/math")
+def math():	
+	result = None
+	num1 = int(request.args.get('num1'))
+	num2 = int(request.args.get('num2'))
+
+	if request.args.get('math') == "add": 
+		result = "the sum is {}".format(num1 + num2)
+
+	elif request.args.get('math') == "subtract":
+		 result= "the difference is {}".format(num1 - num2)
+	
+	elif request.args.get('math') == "multiply":
+		result= "the product is {}".format(num1 * num2)
+
+	elif request.args.get('math')== "divide":
+		result = "the quotient is {}".format(num1 / num2)
+	return render_template("math.html", result=result)					
 
 @app.route("/person/<name>/<int:age>")
 def person(name, age):

4e547254412be87346d452e7bb86f2de548a1688
Zejian Shen <zesansan@gmail.com>
2017-11-24 20:44:18 -0800

    adding new and delete

--- Unit-01/04-flask-crud/app.py
+++ Unit-01/04-flask-crud/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, request, render_template, url_for, redirect
 from snack import Snack
 from flask_modus import Modus
 
@@ -7,27 +7,56 @@ snacks=[
 	Snack('beef jerky', 'savory'),
 	Snack('chocolate chip cookies', 'sweet'),
 	Snack('granola bar', 'health')
-
 ]
 
 app = Flask(__name__)
 modus = Modus(app)
 
-@app.route('/snacks', methods=["GET", "POST"])
+@app.route('/snacks', methods=['GET','POST'])
 def index():
-	return render_template('index.html', snacks = snacks)
+	if request.method == 'POST':
+		new_snack = Snack(
+			request.form.get('snack_name'), 
+			request.form.get('snack_kind')
+			)
+		snacks.append(new_snack)
+		return redirect(url_for('index'))	
+	return render_template('index.html', snacks=snacks)
 
 @app.route('/snacks/new')
 def new():
 	return render_template('new.html')
 
-@app.route('/snacks/:id')
-def show():
-	return render_template('show.html')
+@app.route('/snacks/<int:id>', methods=['GET', 'DELETE', 'PATCH'])
+def show(id):
 
-@app.route('/snacks/:id/edit')
-def edit():
-	return render_template('edit.html')			
+	found_snack = [
+		snack 
+		for snack in snacks 
+		if snack.id == id
+	][0]
+
+	if request.method == b'PATCH':
+		found_snack.name = request.form.get('snack_name')
+		return redirect(url_for('index'))
+
+	if request.method == b'DELETE': 
+		snacks.remove(found_snack)
+
+	if found_snack and request.method == 'GET':	
+		return render_template('show.html', snack=found_snack)
+	return redirect(url_for('index'))	
+
+@app.route('/snacks/<int:id>/edit')
+
+def edit(id):
+	found_snack = [
+		snack 
+		for snack in snacks 
+		if snack.id == id
+		][0]
+	
+	return render_template('edit.html', snack=found_snack)			
 
 if __name__ == '__main__':
 	app.run(debug=True)	

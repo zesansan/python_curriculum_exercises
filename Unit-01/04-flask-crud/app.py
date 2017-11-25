30b1294928e89fa2d18b044194322c235b3b74be
Zejian Shen <zesansan@gmail.com>
2017-11-25 12:43:43 -0800

    updated variable names to match test, still not passing delete, edit and show tests- will need to check on Monday

--- Unit-01/04-flask-crud/app.py
+++ Unit-01/04-flask-crud/app.py
@@ -2,11 +2,7 @@ from flask import Flask, request, render_template, url_for, redirect
 from snack import Snack
 from flask_modus import Modus
 
-snacks=[
-	Snack('potato chips', 'savory'),
-	Snack('beef jerky', 'savory'),
-	Snack('chocolate chip cookies', 'sweet'),
-	Snack('granola bar', 'health')
+snack_list=[
 ]
 
 app = Flask(__name__)
@@ -16,12 +12,12 @@ modus = Modus(app)
 def index():
 	if request.method == 'POST':
 		new_snack = Snack(
-			request.form.get('snack_name'), 
-			request.form.get('snack_kind')
+			request.values.get('name'), 
+			request.values.get('kind')
 			)
-		snacks.append(new_snack)
+		snack_list.append(new_snack)
 		return redirect(url_for('index'))	
-	return render_template('index.html', snacks=snacks)
+	return render_template('index.html', snacks=snack_list)
 
 @app.route('/snacks/new')
 def new():
@@ -30,17 +26,17 @@ def new():
 @app.route('/snacks/<int:id>', methods=['GET', 'DELETE', 'PATCH'])
 def show(id):
 
-	found_snack = [
+	found_snack = next(
 		snack 
-		for snack in snacks 
+		for snack in snack_list 
 		if snack.id == id
-	][0]
+	)
 
 	if request.method == b'PATCH':
 		found_snack.name = request.form.get('snack_name')
 
 	if request.method == b'DELETE': 
-		snacks.remove(found_snack)
+		snack_list.remove(found_snack)
 
 	if found_snack and request.method == 'GET':	
 		return render_template('show.html', snack=found_snack)
@@ -49,11 +45,11 @@ def show(id):
 @app.route('/snacks/<int:id>/edit')
 
 def edit(id):
-	found_snack = [
+	found_snack = next(
 		snack 
-		for snack in snacks 
+		for snack in snack_list 
 		if snack.id == id
-		][0]
+		)
 	
 	return render_template('edit.html', snack=found_snack)			
 

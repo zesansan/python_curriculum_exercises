6b06bb47586b92c6a046bb1136ab484c452d0c20
Zejian Shen <zesansan@gmail.com>
2017-11-30 13:43:52 -0800

    adding RESTful routs to users and messages, created users-messages-db

--- Unit-01/07-sql-alchemy-2/app.py
+++ Unit-01/07-sql-alchemy-2/app.py
@@ -3,26 +3,62 @@ from flask_modus import Modus
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/users-db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/users-messages-db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 modus = Modus(app)
 
+class User(db.Model):
+
+	__tablename__ = 'users'
+
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.Text)
+	first_name = db.Column(db.Text)
+	last_name = db.Column(db.Text)
+	messages = db.relationship('Message', backref = 'user', lazy='dynamic', cascade='all,delete')
+
+	def __init__(self, username, first_name, last_name):
+		self.username = username
+		self.first_name = first_name
+		self.last_name = last_name
+	
+	def __repr__(self):
+		return "{}'s name is {} {}.".format(self.username, self.first_name, self.last_name)
+
+class Message(db.Model):
+
+	__tablename__ = 'messages'
+
+	id = db.Column(db.Integer, primary_key=True)
+	text = db.Column(db.Text)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+	def __init__(self, text, user_id):
+		self.text = text
+		self.user_id = user_id
+
+@app.route('/')
+def root():
+	return render_template('users/index.html')
+
 @app.route('/users')
 def index():
-	return render_template('index.html')
+	return render_template('users/index.html')
 
 @app.route('/users/new')
 def new():
-	return render_template('new.html')
+	return render_template('users/new.html')
 
-@app.route('/users/<in:user_id>')
-def show(user_id):
-	return render_template('show.html')
+@app.route('/users/<int:id>')
+def show(id):
+	return render_template('users/show.html')
 
-@app.route('/users/<in:user_id>/edit')
-def edit(user_id):
-	return render_template('edit.html')			
+@app.route('/users/<int:id>/edit')
+def edit(id):
+	return render_template('users/edit.html')
+
+@app.route('/users/<int')				
 
 if __name__ == '__main__':
 	app.run(debug=True)	

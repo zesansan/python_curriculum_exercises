0e6850d6f70a811b7dbd8f1885a6d25cc6b0f211
Zejian Shen <zesansan@gmail.com>
2017-12-01 14:39:30 -0800

    setting up files with mvc

---
+++ Unit-02/01-blueprints/project/messages/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators 
+
+class UserForm(FlaskForm):
+	username = StringField('Username', [validators.DataRequired()])
+	first_name = StringField('First Name', [validators.DataRequired()])
+	last_name = StringField('Last Name', [validators.DataRequired()])
+
+class MessageForm(FlaskForm):
+	text = StringField('Message', [validators.DataRequired()])
+
+class DeleteForm(FlaskForm):
+	pass	

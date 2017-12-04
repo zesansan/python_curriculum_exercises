c2c1b2fad0ce15311f146bac215500c207e859e1
Zejian Shen <zesansan@gmail.com>
2017-12-04 14:28:54 -0800

    refactoring users/messages

--- Unit-02/01-blueprints/project/messages/views.py
+++ Unit-02/01-blueprints/project/messages/views.py
@@ -1,15 +1,14 @@
-from flask import Blueprint, redirect, render_template, url_for, request
+from flask import Blueprint, redirect, render_template, url_for, request, flash
 from project.messages.models import Message
 from project.users.models import User
-from project.messages.forms import MessageForm, DeleteForm
+from project.messages.forms import MessageForm
 from project import db
 
 messages_blueprint = Blueprint(
-	'messages', __name__, template_folder = 'templates/messages')
+	'messages', __name__, template_folder = 'templates')
 
-@messages_blueprint.route('/users/<int:user_id>/messages', methods = ['GET', 'POST'])
-def messages_index(user_id):
-	delete_form = DeleteForm()
+@messages_blueprint.route('/', methods = ['GET', 'POST'])
+def index(user_id):
 	user = User.query.get(user_id)
 	if request.method == "POST":
 		form = MessageForm(request.form)
@@ -17,18 +16,19 @@ def messages_index(user_id):
 			new_message = Message(request.form['text'], user_id)
 			db.session.add(new_message)
 			db.session.commit()
-			return redirect(url_for('messages.messages_index', user_id = user_id))
-		return render_template('messages_new.html', user=user, form=form)
-	return render_template('messages/index.html', user=user, delete_form = delete_form)
+			flash("message added")
+			return redirect(url_for('messages.index', user_id = user_id))
+		return render_template('messages/new.html', user=user, form=form)
+	return render_template('messages/index.html', user=user)
 		
-@messages_blueprint.route('/users/<int:user_id>/messages/new')
-def messages_new(user_id):
+@messages_blueprint.route('/new')
+def new(user_id):
 	found_user = User.query.get(user_id)
 	form = MessageForm()
 	return render_template('messages/new.html', user= found_user, form=form)
 
-@messages_blueprint.route('/users/<int:user_id>/messages/<int:id>', methods=['PATCH', 'GET', 'DELETE'])
-def messages_show(user_id, id):
+@messages_blueprint.route('/<int:id>', methods=['PATCH', 'GET', 'DELETE'])
+def show(user_id, id):
 	found_message = Message.query.get(id)
 	if request.method == b'PATCH':
 		form=MessageForm(request.form)
@@ -36,19 +36,19 @@ def messages_show(user_id, id):
 			found_message.text = request.form['text']
 			db.session.add(found_message)
 			db.session.commit()
-			return redirect(url_for('messages.messages_index', user_id = user_id))
+			flash('message updated')
+			return redirect(url_for('messages.index', user_id = user_id))
 		return render_template('messages/edit.html', message=found_message,form=form)	
 	if request.method == b'DELETE':
-		delete_form=DeleteForm(request.form)
-		if delete_form.validate():
-			db.session.delete(found_message)
-			db.session.commit()
-		return redirect(url_for('messages.messages_index', user_id=user_id))
+		db.session.delete(found_message)
+		db.session.commit()
+		flash('message deleted')
+		return redirect(url_for('messages.index', user_id=user_id))
 	return render_template('messages/show.html', message = found_message)	
 
-@messages_blueprint.route('/users/<int:user_id>/messages/<int:id>/edit')
-def messages_edit(user_id, id):
+@messages_blueprint.route('/<int:id>/edit')
+def edit(user_id, id):
 	found_message = Message.query.get(id)
 	form=MessageForm(obj=found_message)
-	delete_form = MessageForm()
-	return render_template('messages/edit.html', message=found_message, form=form, delete_form=delete_form)
+	return render_template('messages/edit.html', message=found_message, form=form)
+
--- Unit-02/01-blueprints/project/users/views.py
+++ Unit-02/01-blueprints/project/users/views.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, redirect, render_template, url_for, request, flash
 from project.users.models import User
-from project.users.forms import UserForm, DeleteForm
+from project.users.forms import UserForm
 from project import db
 
-users_blueprint = Blueprint('users', __name__, template_folder='templates')
+users_blueprint = Blueprint('users', __name__, template_folder='templates/users')
 
-@users_blueprint.route('/users', methods=['POST', 'GET'])
+@users_blueprint.route('/', methods=['POST', 'GET'])
 def index():
 	if request.method=='POST':
 		form = UserForm(request.form)
@@ -13,17 +13,17 @@ def index():
 			new_user = User(request.form['username'],request.form['first_name'],request.form['last_name'])
 			db.session.add(new_user)
 			db.session.commit()
+			flash('User created!')
 			return redirect(url_for('users.index'))	
-		else:
-			return render_template('user/new.html', form=form)	
-	return render_template('users/index.html', users=User.query.all())
+		return render_template('new.html', form=form)	
+	return render_template('index.html', users=User.query.all())
 
-@users_blueprint.route('/users/new')
+@users_blueprint.route('/new')
 def new():
 	user_form = UserForm()
-	return render_template('users/new.html', form=user_form)
+	return render_template('new.html', form=user_form)
 
-@users_blueprint.route('/users/<int:id>', methods=['GET','PATCH','DELETE'])
+@users_blueprint.route('/<int:id>', methods=['GET','PATCH','DELETE'])
 def show(id):
 	found_user = User.query.get(id)
 	if request.method == b'PATCH':
@@ -36,19 +36,18 @@ def show(id):
 			db.session.commit()
 			flash('User updated!')
 			return redirect(url_for('users.index'))
-		return render_template('users/edit.html', user=found_user, form=form)	
+		return render_template('edit.html', user=found_user, form=form)	
 	if request.method ==b'DELETE':
-		delete_form = DeleteForm()
-		if delete_form.validate():
-			db.session.delete(found_user)
-			db.session.commit()
-			flash('User deleted!')
+		db.session.delete(found_user)
+		db.session.commit()
+		flash('User deleted!')
 		return redirect(url_for('users.index'))
-	return render_template('users/show.html', user=found_user)
+	return render_template('show.html', user=found_user)
 
-@users_blueprint.route('/users/<int:id>/edit')
+@users_blueprint.route('/<int:id>/edit')
 def edit(id):
-	delete_form = DeleteForm()
 	user=User.query.get(id)
 	user_form = UserForm(obj=user) #use obj to prepopulate forms
-	return render_template('users/edit.html', user=user, form=user_form,delete_form=delete_form)	
+	return render_template('edit.html', user=user, form=user_form)	
+
+

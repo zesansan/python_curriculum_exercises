db7e8841e1cb713cbf188f7f5953b27519120221
Zejian Shen <zesansan@gmail.com>
2017-12-04 20:21:34 -0800

    creating db and migration

---
+++ Unit-02/02-many-to-many/project/models.py
@@ -0,0 +1,40 @@
+from project import db
+
+EmployeeDepartment = db.Table('emplyee_departments', 
+								db.Column('id',
+											db.Integer,
+											primary_key=True),
+								db.Column('employee_id', 
+											db.Integer,
+											db.ForeignKey('employees.id', ondelete="cascade")),
+								db.Column('department_id',
+											db.Integer,
+											db.ForeignKey('departments.id', ondelete="cascade")))
+class Employee(db.Model):
+	__tablename__ ='employees'
+
+	id = db.Column(db.Integer,primary_key=True)
+	name = db.Column(db.Text)
+	years_at_company = db.Column(db.Integer)
+	departments = db.relationship("Department",
+									secondary=EmployeeDepartment,
+									backref=db.backref('employees'))
+
+	def __init__(self,name,years_at_company):
+		self.name = name
+		self.years_at_company = years_at_company
+
+	def __repr__(self):
+		return "{} has been at the company for {} years.".format(self.name,self.years_at_company)
+
+class Department(db.Model):
+	__tablename__ = 'departments'
+
+	id = db.Column(db.Integer,primary_key=True)
+	name = db.Column(db.Text)
+
+	def __init__(self,name):
+		self.name = name
+
+	def __repr__(self):
+		return "{} is the deparment.".format(self.name)	

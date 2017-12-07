9cd3dd08d08c466b973f9c8a2549541ecc403013
Zejian Shen <zesansan@gmail.com>
2017-12-06 18:25:36 -0800

    full crud on departments, invidual departments list all employees

--- Unit-02/02-many-to-many/project/departments/views.py
+++ Unit-02/02-many-to-many/project/departments/views.py
@@ -24,13 +24,25 @@ def new():
 	form = NewDepartmentForm()
 	return render_template('departments/new.html', form=form, departments=Department.query.all())	
 
-@departments_blueprint.route('/<int:id>')
+@departments_blueprint.route('/<int:id>', methods = ['GET', 'PATCH', 'DELETE'])
 def show(id):
 	found_department = Department.query.get(id)
-	#employees = Department.employees.query.get(id)
-	return render_template('departments/show.html', department = found_department)
+	form = NewDepartmentForm()
+	delete_form = DeleteForm()
+	if request.method == b'PATCH':
+		found_department.name = request.form['name']
+		db.session.add(found_department)
+		db.session.commit()
+		return redirect(url_for('departments.index'))
+	if request.method == b'DELETE':
+		db.session.delete(found_department)
+		db.session.commit()
+		return redirect(url_for('departments.index'))
+	return render_template('departments/show.html', department = found_department, form=form, delete_form = delete_form)
 
 @departments_blueprint.route('/<int:id>/edit')
 def edit(id):
 	found_department = Department.query.get(id)
-	return render_template('departments/edit.html', department = found_department)		
+	form = NewDepartmentForm()
+	delete_form = DeleteForm()
+	return render_template('departments/edit.html', department = found_department, form=form, delete_form=delete_form)		

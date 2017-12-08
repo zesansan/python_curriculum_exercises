4becfdb357aa9ad98c984bd624ecff57af61c6da
Zejian Shen <zesansan@gmail.com>
2017-12-07 21:29:11 -0800

    refactored __init__.py with flask login

--- Unit-02/04-flask-login/project/__init__.py
+++ Unit-02/04-flask-login/project/__init__.py
@@ -2,11 +2,16 @@ from flask import Flask, request, redirect, url_for
 from flask_modus import Modus
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 import os
 
+login_manager = LoginManager()
 app = Flask(__name__)
 modus = Modus(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/user-message-auth'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -19,6 +24,15 @@ from project.messages.views import messages_blueprint
 
 app.register_blueprint(users_blueprint, url_prefix='/users')
 app.register_blueprint(messages_blueprint, url_prefix='/users/<int:user_id>/messages')
+
+login_manager.login_view = "users.login"
+
+from project.models import User
+
+@login_manager.user_loader
+def load_user(id):
+	return User.query.get(int(id))
+
 
 @app.route('/')
 def root():
